9e8fc69a4c2ef84a2ad869cca8264e854d490b5b
cryptogosu <github@cryptogosu.com>
2021-05-18 22:33:38 -0500

    Updated routes best practices

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var healthRouter = require('./routes/health');
+var healthRoute = require('./routes/health');
 
 var app = express();
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/health', healthRouter);
+app.use('/health', healthRoute);
 
 app.listen(8080, () => {
   console.log(`Stats-BFF Started Successfully`);
--- app/routes/health.js
+++ app/routes/health.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+async function healthCheck(req, res) {
   res.send('Stats-BFF is healthy');
-});
+}
 
-module.exports = router;
+module.exports = healthCheck;
