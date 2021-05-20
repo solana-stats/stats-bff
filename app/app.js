a6f2232eb58376661d37e809822351a497010514
cryptogosu <github@cryptogosu.com>
2021-05-19 23:35:50 -0500

    Updated healthcheck

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/health', healthRoute);
+app.use('/stats-bff/health', healthRoute);
 
 app.listen(8080, () => {
   console.log(`Stats-BFF Started Successfully`);
