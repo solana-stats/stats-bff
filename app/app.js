7d7007e25df0475ade9285b449511d399638e21e
cryptogosu <github@cryptogosu.com>
2021-06-07 23:59:54 -0500

    Configured CORS

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var healthRoute = require('./routes/health');
 
@@ -12,6 +13,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use('/stats-bff/health', healthRoute);
 
