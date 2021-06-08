1071921649fd8b435f3cf5cfd59347232ebc684a
cryptogosu <github@cryptogosu.com>
2021-06-07 19:31:31 -0500

    Debugging 504 issue

--- app/routes/fees.js
+++ app/routes/fees.js
@@ -1,11 +1,18 @@
 const client = require('../config/db.config')
 
 async function getDailyFees(req, res) {
+    console.log('creating query');
     let query = `SELECT SUM(fee_amt) from stats.block_stats where block_time < now() and block_time > now() - interval '1 Day';`
-    let dbResponse = await client.query(query);
-    res.send({
-        fees: dbResponse.rows[0].sum
-    });
+    console.log('sending query');
+    try {
+        let dbResponse = await client.query(query);
+        console.log('response received');
+        res.send({
+            fees: dbResponse.rows[0].sum
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports = getDailyFees
